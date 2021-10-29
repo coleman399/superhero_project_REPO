4e0ccf2d00739d345bc9e3d2e31186133706dbac
Dillon Coleman <coleman399@gmail.com>
2021-10-29 14:55:00 -0700

    added picture of favorite hero

--- superhero_project/superhero_project/urls.py
+++ superhero_project/superhero_project/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('superheroes/', include('superheroes.urls')),
     path('', RedirectView.as_view(url = '/superheroes/')),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
